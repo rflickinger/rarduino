5ff03ceb09945269741f0de11454c7efb9bfc76c
rflickinger <rflickinger15@gmail.com>
2025-05-22 22:33:39 -0400

    Fixed broken config import

--- pi/app/__init__.py
+++ pi/app/__init__.py
@@ -6,7 +6,7 @@ from apispec_webframeworks.flask import FlaskPlugin
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object("Config")
+    app.config.from_object(Config)
 
     db.init_app(app)
     migrate.init_app(app, db)
